3753aa3e39c94a433ca241e6f0d8aac6eb7bb550
rafasantana_meli <rafaela.santana@mercadolivre.com>
2024-05-06 14:47:11 -0300

    atualizacao

--- src/main/java/com/mercadolibre/Main.java
+++ src/main/java/com/mercadolibre/Main.java
@@ -2,6 +2,15 @@ package com.mercadolibre;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Pessoa rafa = new Pessoa("Rafa", 26, "Rafaela@rafa.com");
+        System.out.println("titular " + rafa.getNome());
+        System.out.println("idade " + rafa.getIdade());
+        System.out.println("email " + rafa.getEmail());
+
+        ContaBancaria c1 = new ContaBancaria(1, "Rafa", 1.50);
+        System.out.println("saldo inicial "+ c1.verificarSaldo());
+        c1.sacar(1);
+        c1.depositar(3.75);
+        System.out.println("saldo final "+ c1.verificarSaldo());
     }
 }
