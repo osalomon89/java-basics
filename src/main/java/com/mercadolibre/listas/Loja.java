ecbeeff5c242a4e5a7e5f41a08e0d12008cf2cff
rafasantana_meli <rafaela.santana@mercadolivre.com>
2024-05-10 09:57:20 -0300

    atualizaçao Pasta listas

--- src/main/java/com/mercadolibre/listas/Loja.java
+++ src/main/java/com/mercadolibre/listas/Loja.java
@@ -1,26 +1,48 @@
 package com.mercadolibre.listas;
 
-import com.mercadolibre.heranca.Funcionario;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Loja {
     public static void main(String[] args){
 
-        List<Produtos> produto = new ArrayList<>();
-       Produtos chocolate = new Produtos(01, "chocolate", 7.00);
-        Produtos bala = new Produtos(02, "bala", 0.10);
-        Produtos chiclete = new Produtos(03, "chiclete", 0.20);
+        List<Produto> produtos = new ArrayList<>();
 
-        produto.add(chiclete);
-        produto.add(chocolate);
-        produto.add(bala);
+        Produto chocolate = new Produto("chocolate", 7.00);
+        Produto bala = new Produto("bala", 0.10);
+        Produto chiclete = new Produto("chiclete", 0.20);
 
-        produto.forEach();
+        produtos.add(chiclete);
+        produtos.add(chocolate);
+        produtos.add(bala);
 
+        double valorMinimo = 0.15;
 
+//        for (int i = 0; i < produtos.size(); i++) {
+//            Produto elemento = produtos.get(i);
+//            if (elemento.getPreco() >= valorMinimo){
+//                System.out.println(elemento.getNome());
+//            }
+//        }
 
+        for (Produto produto : produtos) {
+            System.out.println(produto.getNome());
+        }
+
+        System.out.println("++++++++++++++++++++++++++");
+
+        for (int i = 0; i < produtos.size(); i++) {
+            if (produtos.get(i).getPreco() < valorMinimo){
+                produtos.remove(i);
+            }
+        }
+
+        for (int i = 0; i < produtos.size(); i++) {
+            System.out.println(produtos.get(i).getNome());
+        }
+
+        System.out.println("---------Fin");
 
     }
 }
+
