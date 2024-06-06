2cfa18f321d1ed61af0e062b76c627d6b932eb84
osalomon89 <ext_ossalomo@mercadolibre.com>
2024-06-06 10:25:51 -0300

    test

--- src/main/java/com/mercadolibre/Main.java
+++ src/main/java/com/mercadolibre/Main.java
@@ -3,9 +3,9 @@ package com.mercadolibre;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        //SpringApplication.run(Main.class, args);
     }
 }
---
+++ src/main/java/com/mercadolibre/Product.java
@@ -0,0 +1,7 @@
+package com.mercadolibre;
+
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+}
